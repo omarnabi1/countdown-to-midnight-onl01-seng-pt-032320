293a0c2122ec55e270ecec0e6645db370f14af04
Omar Nabi <onabi20@gmail.com>
2020-03-26 07:09:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 def countdown(seconds)
-  while seconds > 0 
-  puts "#{seconds} SECOND(S)!"
-  seconds -= 1 
-end
-return "HAPPY NEW YEAR!"
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
 
